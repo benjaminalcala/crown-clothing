f83ad24939a8dbd683490e5866b481f6a8dd2dc5
Benjamin Alcala <benalcalajr@gmail.com>
2020-10-21 17:55:29 -0700

    added header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
-import HomePage from './pages/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
